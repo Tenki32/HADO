a13ffbd0c95d99be3d966577f918ff97e0a64ca2
Tenki32 <ilya.stepanenko.20000@mail.ru>
2025-02-19 21:38:06 +0300

    Update daa.js

--- daa.js
+++ daa.js
@@ -1,13 +1,10 @@
-// 1. Находим элемент meta по атрибуту name="description"
+
 const metaDescription = document.querySelector('meta[name="description"]');
 
-// 2. Проверяем, что элемент найден
-if (metaDescription) {
-  // 3. Изменяем значение атрибута content
-  metaDescription.setAttribute('content', 'Новое описание для метатега description.');
 
-  // Альтернативный способ изменения значения атрибута content:
-  // metaDescription.content = 'Другое новое описание.';
+if (metaDescription) {
+
+  metaDescription.setAttribute('content', 'Новое описание для метатега description.');
 
   console.log('Описание успешно изменено!');
 } else {
